687236b3591fa0ee3d400e8069001e37d2230de1
rehanashraf <rehanashraf@rehans-mbp.gateway.pace.com>
2016-07-17 21:37:09 -0700

    Rehan added the picture and other custom button for fb

--- Vigor/MainScreenViewController.swift
+++ Vigor/MainScreenViewController.swift
@@ -24,8 +24,11 @@ class MainScreenViewController: UIViewController, FBSDKLoginButtonDelegate {
 //        loginButton.center = view.center
 //        loginButton.delegate = self
         
-        if FBSDKAccessToken.currentAccessToken() != nil{
-            fetchProfile()
+
+        if let token = FBSDKAccessToken.currentAccessToken() {
+//            fetchProfile()
+            performSegueWithIdentifier("FBHomeView", sender: nil)
+            print("in view did load")
         }
 
         // Do any additional setup after loading the view.
@@ -38,6 +41,19 @@ class MainScreenViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
         print("Facebook Login Completed")
+        print(result.token.tokenString) //YOUR FB TOKEN
+        let req = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"], tokenString: result.token.tokenString, version: nil, HTTPMethod: "GET")
+        req.startWithCompletionHandler({ (connection, result, error : NSError!) -> Void in
+            if(error == nil)
+            {
+                print("result \(result)")
+            }
+            else
+            {
+                print("error \(error)")
+            }
+        })
+        performSegueWithIdentifier("FBHomeView", sender: nil)
  //       fetchProfile()
     }
     
@@ -67,6 +83,29 @@ class MainScreenViewController: UIViewController, FBSDKLoginButtonDelegate {
             if let picture = result["picture"] as? NSDictionary, data = picture["data"] as? NSDictionary, url = data["url"] as? String{
                 print("The url where the picture is at ", url)
             }
+        }
+    }
+    @IBAction func loginFacebookAction(sender: AnyObject) {
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logInWithReadPermissions(["email"], fromViewController: self) { (result, error) -> Void in
+            if (error == nil){
+                let fbloginresult : FBSDKLoginManagerLoginResult = result
+                if(fbloginresult.grantedPermissions.contains("email"))
+                {
+                    self.getFBUserData()
+                }
+            }
+        }
+    }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.currentAccessToken()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).startWithCompletionHandler({ (connection, result, error) -> Void in
+                if (error == nil){
+                    //everything works print the user data
+                    print(result)
+                }
+            })
         }
     }
     /*
