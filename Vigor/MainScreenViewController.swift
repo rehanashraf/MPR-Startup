b4664ad82de895b36c833640c24fbc94c0a07f3e
rehanashraf <rehanashraf@rehans-mbp.gateway.pace.com>
2016-07-17 20:32:46 -0700

    Rehan added facebooklogin, core and health and health ui kit

--- Vigor/MainScreenViewController.swift
+++ Vigor/MainScreenViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
-class MainScreenViewController: UIViewController {
+class MainScreenViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    
+    let loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        view.addSubview(loginButton)
+//        loginButton.center = view.center
+//        loginButton.delegate = self
+        
+        if FBSDKAccessToken.currentAccessToken() != nil{
+            fetchProfile()
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +36,39 @@ class MainScreenViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("Facebook Login Completed")
+        fetchProfile()
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func loginButtonWillLogin(loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    func fetchProfile()
+    {
+        print("Current profile Fetched")
+        let parameters = ["fields": "email, first_name, last_name, picture.type(large)"]
+        
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).startWithCompletionHandler { (connection, result, error) -> Void in
+            if error != nil{
+                print("printing error",error)
+                return
+            }
+            if let email = result ["email"] as? String {    // prathi here is the email we can use this to store in the database.
+                
+                print("Email is ",email, ".")
+            }
+            
+            if let picture = result["picture"] as? NSDictionary, data = picture["data"] as? NSDictionary, url = data["url"] as? String{
+                print("The url where the picture is at ", url)
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
